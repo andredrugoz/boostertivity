ed4777831d6c0102bf0b53f59a4c6448fd447f90
AndreaN <andrea.nardi@checkupdigitale.com>
2023-01-04 15:57:41 +0100

    Pricing Plan update

--- components/PricingPlan/PricingPlanStyle3.js
+++ components/PricingPlan/PricingPlanStyle3.js
@@ -29,20 +29,19 @@ const PricingPlanStyle3 = () => {
                                 <ul className="features-list">
                                     <li><i className="ri-check-line"></i> Videocall 30m</li>
                                     <li><i className="ri-check-line"></i> Presentazione</li>
-                                    <li><i className="ri-check-line"></i> Focus sulle criticità</li>
                                 </ul>
                             </div>
                         </div>
 
-                        {/* <div className="col-lg-4 col-md-6 col-sm-6">
+                        <div className="col-lg-4 col-md-6 col-sm-6">
                             <div className="single-pricing-box active">
                                 <div className="title">
-                                    <h3>Interactive</h3>
+                                    <h3>Advanced</h3>
                                     <p>Il migliore per iniziare seriamente</p>
                                 </div>
                                 <span className="popular">La più scelta</span>
                                 <div className="price">
-                                    € 195
+                                    A partire da 295€
                                 </div>
                                 
                                 <Link href="/sign-in">
@@ -50,16 +49,12 @@ const PricingPlanStyle3 = () => {
                                 </Link>
 
                                 <ul className="features-list">
-                                    <li><i className="ri-check-line"></i> Up to 200 Website</li>
-                                    <li><i className="ri-check-line"></i> Lifetime free Support</li>
-                                    <li><i className="ri-check-line"></i> 25 GB Dedicated Hosting free</li>
-                                    <li><i className="ri-check-line"></i> 24/7 Support</li>
-                                    <li><i className="ri-check-line"></i> Business Contact</li>
-                                    <li><i className="ri-check-line"></i> SEO Optimized</li>
-                                    <li><i className="ri-close-line"></i> Live Support</li>
+                                    <li><i className="ri-check-line"></i> Videocall su appuntamento</li>
+                                    <li><i className="ri-check-line"></i> Focus sulle criticità</li>
+                                    <li><i className="ri-check-line"></i> Creazione piano d'azione</li>
                                 </ul>
                             </div>
-                        </div> */}
+                        </div>
 
                         <div className="col-lg-4 col-md-6 col-sm-6">
                             <div className="single-pricing-box">
@@ -68,7 +63,7 @@ const PricingPlanStyle3 = () => {
                                     <p>Un piano d'azione ad hoc</p>
                                 </div>
                                 <div className="price">
-                                    Su richiesta
+                                    A partire da 395€
                                 </div>
 
                                 <Link href="/sign-in">
